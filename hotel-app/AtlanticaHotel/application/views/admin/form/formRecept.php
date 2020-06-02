0e5cd6eff4adaa50ce62fd6923249e3ebb845949
Shinta Audia <shintaaudia75@student.uns.ac.id>
2020-06-02 09:57:08 +0700

    hapus

--- hotel-app/AtlanticaHotel/application/views/admin/form/formRecept.php
+++
@@ -1,4 +0,0 @@
-<div class="row d-flex justify-content-between border-bottom mb-3">
-    <h2><?php echo $page ?></h2>
-    <a href="<?php echo base_url() ?>admin/view_data/data_recept"><button class="btn text-white mb-3" style="background-color: #273c75;">Data Resepsionis <i class="fas fa-arrow-left    "></i></i></button></a>
-</div>
